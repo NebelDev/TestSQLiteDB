039528618c4ca708c46dcd3c26739b6a0b0580a2
NebelDev <asd@asd.it>
2016-06-06 15:05:18 +0200

    -

--- app/src/main/java/com/example/testgallery/testsqlitedb/MainActivity.java
+++ app/src/main/java/com/example/testgallery/testsqlitedb/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DatabaseHelper db = new DatabaseHelper(this);
-        String a;
     }
 }
