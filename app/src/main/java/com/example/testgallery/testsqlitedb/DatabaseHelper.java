a08ad7157181afc9875ba9c3ee76ec7529c132b5
Davide <asd@asd.it>
2016-06-06 14:40:01 +0200

    -

---
+++ app/src/main/java/com/example/testgallery/testsqlitedb/DatabaseHelper.java
@@ -0,0 +1,27 @@
+package com.example.testgallery.testsqlitedb;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    //Generic info
+    private static final String DATABASE_NAME = "sqlitedb.sqlite";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
