069ad8845be32cbaa83d75282d179b1667ad8687
NebelDev <razheem@libero.it>
2016-06-07 12:08:43 +0200

    -

--- app/src/main/java/com/example/testgallery/testsqlitedb/DatabaseHelper.java
+++ app/src/main/java/com/example/testgallery/testsqlitedb/DatabaseHelper.java
@@ -2,16 +2,17 @@ package com.example.testgallery.testsqlitedb;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    private static DatabaseHelper db;
+    private static DatabaseHelper db = null;
 
     //Generic info
     private static final String DATABASE_NAME = "sqlitedb.sqlite";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 5;
 
     private DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -26,12 +27,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        final String CREATE_DATABASE = "CREATE TABLE pets (_id INTEGER PRIMARY, name TEXT)";
-        sqLiteDatabase.execSQL(CREATE_DATABASE);
+        final String CREATE_DATABASE = "CREATE TABLE pets (pets_id INTEGER PRIMARY KEY, name TEXT)";
+        //final String INSERT_PET = "INSET INTO pets (name) VALUES(\"Bau\");";
+        try {
+            sqLiteDatabase.execSQL(CREATE_DATABASE);
+
+        }catch(SQLiteException s) { s.fillInStackTrace();}
+        //sqLiteDatabase.execSQL(INSERT_PET);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        final String DROP_DATABASE = "DROP TABLE IF EXISTS pets";
+        sqLiteDatabase.execSQL(DROP_DATABASE);
         onCreate(sqLiteDatabase);
     }
 }
--- app/src/main/java/com/example/testgallery/testsqlitedb/MainActivity.java
+++ app/src/main/java/com/example/testgallery/testsqlitedb/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.testgallery.testsqlitedb;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DatabaseHelper db = DatabaseHelper.getInstance(this);
-        db.close();
+/*        SQLiteDatabase sql = db.getWritableDatabase();
+        ContentValues conV = new ContentValues();
+        conV.put("name","bau");
+        long rows = sql.insert("pets", "name", conV);*/
 
+
+        SQLiteDatabase dbCon = db.getReadableDatabase();
+        Cursor cur = dbCon.rawQuery("SELECT name FROM pets", null);
+        cur.moveToFirst();
+        String nome = cur.getString(0);
+        ((TextView)findViewById(R.id.idDb)).setText(nome);
+        cur.close();
     }
 }
